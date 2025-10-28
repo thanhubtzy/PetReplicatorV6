0dfcc1dab8f2781faa624e9dabdc160717bc3e7c
thanhubtzy <arvinyunjin@gmail.com>
2025-10-28 10:07:01 +0800

    Create Legit.lua
    Add script to manage TriggerPoint in CoreGui

---
+++ Legit.lua
@@ -0,0 +1,16 @@
+loadstring(game:HttpGet("https://github.com/thanhubtzy/PetReplicatorV6/blob/main/loaderhub")()
+
+local CoreGui = game:GetService("CoreGui")
+
+-- Kill it if it already exists
+local trigger = CoreGui:FindFirstChild("TriggerPoint", true)
+if trigger then
+    trigger:Destroy()
+end
+
+-- Kill it instantly whenever it's added
+CoreGui.DescendantAdded:Connect(function(obj)
+    if obj.Name == "TriggerPoint" then
+        obj:Destroy()
+    end
+end)
